7f1aca5ee457c71221bb5fa2ff5aef2da8b3b667
Tim Tröndle <tim.troendle@online.de>
2015-12-30 16:41:12 +0100

    added base project structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+exec(open('forrest/__init__.py').read())
+
+setup(
+    name='forrest',
+    version=__version__,
+    description='A database of GAMS runs.',
+    maintainer='Tim Tröndle',
+    maintainer_email='tim.troendle@online.de',
+    url='https://www.github.com/philipphanemann/forrestgums',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=['sumatra'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering'
+    ]
+)
