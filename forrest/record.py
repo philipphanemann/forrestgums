7c5c1582f729130055efb5b3e8253d2c79c3635b
Tim Tröndle <tim.troendle@online.de>
2016-01-23 00:33:53 +0100

    reading solver version from listing

--- forrest/record.py
+++ forrest/record.py
@@ -1,11 +1,32 @@
 """This module contains all extensions of sumatra.records.Record necessary for GAMS."""
 
-# --- Executing XPRESS: elapsed 0:00:00.061
+from sumatra.programs import version_in_command_line_output
 
 
 def extend_sumatra_record_with_gams_metadata(record):
+    """Reads meta data from GAMS listing and ammends the sumatra record with it."""
     record.solver = 'unknown'
-    for line in record.stdout_stderr.splitlines():
+    record.solver_version = 'unknown'
+    sumatra_record = record.to_sumatra()
+    listing_data_key = sumatra_record.output_data[0]
+    listing = sumatra_record.datastore.get_data_item(listing_data_key).content.decode('utf-8')
+    for line in listing.splitlines():
         if line.strip().startswith('SOLVER '):
             record.solver = [word for word in line.split(' ') if word][1]
+    if record.solver != 'unknown':
+        for line in listing.splitlines():
+            if record.solver.lower() in line.lower() and 'version' in line.lower():
+                record.solver_version = version_in_command_line_output(line)
+    if record.solver.lower() == 'cplex':
+        for line in listing.splitlines():
+            if line.lower().startswith('cplex') and len(line.split(' ')) == 2:
+                record.solver_version = line.split(' ')[1]
+    elif record.solver.lower() == 'conopt':
+        for line in listing.splitlines():
+            if record.solver.lower() in line.lower().replace(' ', '') and 'version' in line.lower():
+                record.solver_version = line.split('version')[-1].strip()
+    elif record.solver.lower() == 'xpress':
+        for line in listing.splitlines():
+            if line.startswith('Xpress-Optimizer'):
+                record.solver_version = [word for word in line.split(' ') if word.startswith('v')][0][1:]
     return record
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,9 +2,6 @@ from unittest.mock import Mock
 from pathlib import Path
 from collections import namedtuple
 
-from sumatra.programs import Executable
-from sumatra.versioncontrol import Repository
-from sumatra.launch import LaunchMode
 from sumatra.datastore import DataStore
 from sumatra.records import Record
 import pytest
@@ -12,29 +9,29 @@ import pytest
 from forrest.record import extend_sumatra_record_with_gams_metadata
 
 
-Listing = namedtuple('Listing', ['path_to_file', 'solver'])
+Listing = namedtuple('Listing', ['path_to_file', 'solver', 'solver_version'])
 
 
 LISTING_PATH = Path(__file__).parent / 'resources'
-GUROBI_LISTING = Listing(LISTING_PATH / 'run_gurobi.lst', 'gurobi')
-CPLEX_LISTING = Listing(LISTING_PATH / 'run_cplex.lst', 'cplex')
-BARON_LISTING = Listing(LISTING_PATH / 'run_baron.lst', 'baron')
-CONOPT_LISTING = Listing(LISTING_PATH / 'run_conopt.lst', 'conopt')
-XPRESS_LISTING = Listing(LISTING_PATH / 'run_xpress.lst', 'xpress')
+GUROBI_LISTING = Listing(LISTING_PATH / 'run_gurobi.lst', 'gurobi', '6.0.5')
+CPLEX_LISTING = Listing(LISTING_PATH / 'run_cplex.lst', 'cplex', '12.6.2.0')
+BARON_LISTING = Listing(LISTING_PATH / 'run_baron.lst', 'baron', '15.9.22')
+CONOPT_LISTING = Listing(LISTING_PATH / 'run_conopt.lst', 'conopt', '3.17A')
+XPRESS_LISTING = Listing(LISTING_PATH / 'run_xpress.lst', 'xpress', '28.01.05')
 
 
 @pytest.fixture(params=[GUROBI_LISTING, CPLEX_LISTING, BARON_LISTING, CONOPT_LISTING, XPRESS_LISTING])
 def listing_and_sumatra_record(request):
     listing = request.param
-    record = Record(
-        Mock(spec_set=Executable),
-        Mock(spec_set=Repository),
-        "trnsport.gms",
-        999,
-        Mock(spec_set=LaunchMode),
-        Mock(spec_set=DataStore),
-        stdout_stderr=listing.path_to_file.read_text()
-    )
+    output_data = Mock()
+    output_data.content = listing.path_to_file.read_text().encode('utf-8')
+    datastore = Mock(spec_set=DataStore)
+    datastore.get_data_item.return_value = output_data
+    record = Mock(spec=Record)
+    record.datastore = datastore
+    record.output_data = ['something']
+    record.to_sumatra = Mock()
+    record.to_sumatra.return_value = record
     return listing, record
 
 
@@ -43,3 +40,10 @@ def test_detects_solver(listing_and_sumatra_record):
     sumatra_record = listing_and_sumatra_record[1]
     gams_record = extend_sumatra_record_with_gams_metadata(sumatra_record)
     assert gams_record.solver.lower() == listing.solver
+
+
+def test_detects_solver_version(listing_and_sumatra_record):
+    listing = listing_and_sumatra_record[0]
+    sumatra_record = listing_and_sumatra_record[1]
+    gams_record = extend_sumatra_record_with_gams_metadata(sumatra_record)
+    assert gams_record.solver_version == listing.solver_version
