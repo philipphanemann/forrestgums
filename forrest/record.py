4850b1d8b806bbcaf752f849473ab8755d9ef08d
Tim Tröndle <tim.troendle@online.de>
2016-01-22 23:42:31 +0100

    reading solver name from listing

--- forrest/record.py
+++ forrest/record.py
@@ -1,6 +1,11 @@
 """This module contains all extensions of sumatra.records.Record necessary for GAMS."""
 
+# --- Executing XPRESS: elapsed 0:00:00.061
+
 
 def extend_sumatra_record_with_gams_metadata(record):
-    record.gams = 'jo'
+    record.solver = 'unknown'
+    for line in record.stdout_stderr.splitlines():
+        if line.strip().startswith('SOLVER '):
+            record.solver = [word for word in line.split(' ') if word][1]
     return record
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,6 @@
 from unittest.mock import Mock
+from pathlib import Path
+from collections import namedtuple
 
 from sumatra.programs import Executable
 from sumatra.versioncontrol import Repository
@@ -10,18 +12,34 @@ import pytest
 from forrest.record import extend_sumatra_record_with_gams_metadata
 
 
-@pytest.fixture
-def sumatra_record():
-    return Record(
+Listing = namedtuple('Listing', ['path_to_file', 'solver'])
+
+
+LISTING_PATH = Path(__file__).parent / 'resources'
+GUROBI_LISTING = Listing(LISTING_PATH / 'run_gurobi.lst', 'gurobi')
+CPLEX_LISTING = Listing(LISTING_PATH / 'run_cplex.lst', 'cplex')
+BARON_LISTING = Listing(LISTING_PATH / 'run_baron.lst', 'baron')
+CONOPT_LISTING = Listing(LISTING_PATH / 'run_conopt.lst', 'conopt')
+XPRESS_LISTING = Listing(LISTING_PATH / 'run_xpress.lst', 'xpress')
+
+
+@pytest.fixture(params=[GUROBI_LISTING, CPLEX_LISTING, BARON_LISTING, CONOPT_LISTING, XPRESS_LISTING])
+def listing_and_sumatra_record(request):
+    listing = request.param
+    record = Record(
         Mock(spec_set=Executable),
         Mock(spec_set=Repository),
         "trnsport.gms",
         999,
         Mock(spec_set=LaunchMode),
-        Mock(spec_set=DataStore)
+        Mock(spec_set=DataStore),
+        stdout_stderr=listing.path_to_file.read_text()
     )
+    return listing, record
 
 
-def test_gams_metadata_is_added(sumatra_record):
+def test_detects_solver(listing_and_sumatra_record):
+    listing = listing_and_sumatra_record[0]
+    sumatra_record = listing_and_sumatra_record[1]
     gams_record = extend_sumatra_record_with_gams_metadata(sumatra_record)
-    assert hasattr(gams_record, 'gams')
+    assert gams_record.solver.lower() == listing.solver
