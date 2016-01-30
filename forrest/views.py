a126b250738c82f48a7cefee971928857f95aa67
Tim Tröndle <timtroendle@posteo.de>
2016-01-30 11:37:39 +0100

    added mimetype to lst files

--- forrest/views.py
+++ forrest/views.py
@@ -1,12 +1,22 @@
 import sumatra.records
-from sumatra.recordstore import django_store
 import sumatra.web.views
 
 from forrest.record import extend_django_record_with_gams_metadata
 
 
 class RecordDetailView(sumatra.web.views.RecordDetailView):
+    """A class to add metadata from GAMS result file to record."""
 
     def get_object(self):
         record = super().get_object()
         return extend_django_record_with_gams_metadata(record)
+
+
+class DataDetailView(sumatra.web.views.DataDetailView):
+    """A class to add mimetype to GAMS result files ('*.lst')."""
+
+    def get_object(self):
+        datakey = super().get_object()
+        if datakey.path.endswith('.lst'):
+            datakey.metadata = datakey.metadata.replace('"mimetype": null', '"mimetype": "text/plain"')
+        return datakey
